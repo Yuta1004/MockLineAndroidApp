3417c152a0821b35d2b3555e56ad042d9e79629b
NakagamiYuta <guguru0014@gmail.com>
2018-07-13 16:22:02 +0900

    [add]トークルーム一覧を表示するListViewを追加

--- app/src/main/java/work/nityc_nyuta/mockline/TalkroomViewFragment.kt
+++ app/src/main/java/work/nityc_nyuta/mockline/TalkroomViewFragment.kt
@@ -5,11 +5,28 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
 
 class TalkroomViewFragment : Fragment() {
+    private var layoutView: View? = null
 
+    // FragmentのViewが生成されるときに呼ばれる
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_talkroom_view, container, false)
+        layoutView = inflater.inflate(R.layout.fragment_talkroom_view, container, false)
+        return layoutView
+    }
+
+    // Viewの生成が終了したら呼ばれる
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // トークルーム一覧を表示する
+        val talkroomViewListview = layoutView!!.findViewById<ListView>(R.id.talkroom_view_listview)
+        val talkroomListAdapter = TalkroomViewListAdapter()
+        talkroomListAdapter.setInfo(LayoutInflater.from(activity))
+        talkroomViewListview.adapter = talkroomListAdapter
+
+        talkroomListAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/work/nityc_nyuta/mockline/TalkroomViewListAdapter.kt
@@ -0,0 +1,63 @@
+package work.nityc_nyuta.mockline
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class TalkroomViewListAdapter: BaseAdapter(){
+    // トークルーム一覧を保持するlist
+    private val talkroomList: List<Talkroom>? = listOf(
+            Talkroom("1", "Nakagami"),
+            Talkroom("2", "Kishida"),
+            Talkroom("3", "Hiraki"),
+            Talkroom("4", "Moriue"),
+            Talkroom("5", "Takahashi")
+    )
+
+    private var layoutInflater: LayoutInflater? = null
+
+    // LayoutInflaterをセットする
+    fun setInfo(layoutInflater: LayoutInflater){
+        this.layoutInflater = layoutInflater
+    }
+
+    override fun getCount(): Int {
+        return talkroomList?.size ?: 0
+    }
+
+    override fun getItem(position: Int): Any {
+        return if(talkroomList != null) {
+            talkroomList[position]
+        }else{
+            0
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return if(talkroomList != null){
+            talkroomList[position].id.toLong()
+        }else{
+            0
+        }
+    }
+
+    // ListViewの要素のViewを返す
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val myConvertView = this.layoutInflater!!.inflate(R.layout.talkroom_list_item, parent, false)
+
+        if(talkroomList != null){
+            myConvertView.findViewById<TextView>(R.id.talkroom_name).text = talkroomList[position].name
+        }
+
+        return myConvertView
+    }
+}
+
+// トークルームのデータクラス
+data class Talkroom(val id: String, val name:String)
