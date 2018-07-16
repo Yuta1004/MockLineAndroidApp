718974a280b4e55b9790f5081a4d818677c28dc8
NakagamiYuta <guguru0014@gmail.com>
2018-07-16 17:59:47 +0900

    [add]トークルーム作成処理を追加，作成ボタンのリスナ実装まで
    サーバ通信はまだ

--- app/src/main/java/work/nityc_nyuta/mockline/Activities/MakeTalkroomActivity.kt
+++ app/src/main/java/work/nityc_nyuta/mockline/Activities/MakeTalkroomActivity.kt
@@ -2,6 +2,13 @@ package work.nityc_nyuta.mockline.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
+import android.util.Log
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import work.nityc_nyuta.mockline.Adapters.SelectFriendsListAdapter
 import work.nityc_nyuta.mockline.R
 
 class MakeTalkroomActivity : AppCompatActivity() {
@@ -11,5 +18,27 @@ class MakeTalkroomActivity : AppCompatActivity() {
         setContentView(R.layout.activity_make_talkroom)
 
         title = "トークルーム作成"
+
+        // 選択できる友達一覧をListViewに表示
+        val selectFriendListView = findViewById<ListView>(R.id.select_friend)
+        val adapter = SelectFriendsListAdapter(layoutInflater)
+        selectFriendListView.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        // トークルーム作成ボタンが押された時のリスナ
+        findViewById<Button>(R.id.make_talkroom_button).setOnClickListener {
+            // トークルーム名取得 -> 入力判定
+            val talkroomName = findViewById<TextInputEditText>(R.id.talkroom_name_inp).text.toString()
+            if(talkroomName == ""){
+                findViewById<TextInputLayout>(R.id.talkroom_name_inp_layout)
+                        .error = "トークルーム名が入力されていません"
+                return@setOnClickListener
+            }
+
+            // チェック中のユーザIDを取得して「；」区切りの文字列へ
+            val selectUserIdList = adapter.getSelectUserIDList()
+            var userIdListToStr = ""
+            for(userId in selectUserIdList){ userIdListToStr += "$userId;" }
+        }
     }
 }
---
+++ app/src/main/java/work/nityc_nyuta/mockline/Adapters/SelectFriendsListAdapter.kt
@@ -0,0 +1,86 @@
+package work.nityc_nyuta.mockline.Adapters
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.CheckBox
+import android.widget.TextView
+import work.nityc_nyuta.mockline.R
+import work.nityc_nyuta.mockline.ServerConncection.ServerConnectFriendsData
+import work.nityc_nyuta.mockline.ServerConncection.ServerConnectUserData
+import kotlin.concurrent.thread
+
+class SelectFriendsListAdapter(layoutInflater_args: LayoutInflater): BaseAdapter(){
+    private val friendsList = mutableListOf<Friend>()
+    private val layoutInflater = layoutInflater_args
+
+    init{
+        // スレッド終了監視フラグ
+        var connectionEnd = false
+
+        thread{
+            val friendsIdList = ServerConnectFriendsData().getFriendsList()
+            val userInfo = ServerConnectUserData().getUserData(friendsIdList!!)
+
+            // 取得した友達IDリストをもとにfriendsListを作る
+            for (friendId in friendsIdList) {
+                val friend = userInfo.getJSONObject(friendId)
+                val friendName = friend.getString("name")
+                val friendIconUrl = friend.getString("icon_url")
+                val friendHeaderImageUrl = friend.getString("header_image_url")
+
+                friendsList.add(Friend(friendId, friendName, friendIconUrl, friendHeaderImageUrl))
+            }
+
+            // スレッド終了
+            connectionEnd = true
+        }
+
+        // スレッドが終了するまで待つ
+        while(!connectionEnd){}
+    }
+
+    override fun getCount(): Int {
+        return friendsList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return friendsList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val myConvertView = layoutInflater.inflate(R.layout.select_friend_list_item, parent, false)
+
+        myConvertView.findViewById<TextView>(R.id.name).text = friendsList[position].name
+
+        // チェックボックスが押されるたびにFriendクラスのcheckBoxStatusを変更
+        myConvertView.findViewById<CheckBox>(R.id.select_friend).setOnClickListener {
+            friendsList[position].checkBoxStatus = !friendsList[position].checkBoxStatus
+        }
+
+        return myConvertView
+    }
+
+    // チェックボックスで選択中のユーザID一覧をListで返す
+    fun getSelectUserIDList(findStatus: Boolean=true): List<String>{
+        val retUserIdList = mutableListOf<String>()
+        for(friend in friendsList){
+            if(friend.checkBoxStatus){
+                retUserIdList.add(friend.id)
+            }
+        }
+
+        return retUserIdList.toList()
+    }
+
+    data class Friend(val id: String, val name: String, val icon_url: String, val header_image_url: String,
+                      var checkBoxStatus: Boolean = false)
+}
