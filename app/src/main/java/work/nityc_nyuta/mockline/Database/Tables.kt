f0768976ef1bb9a27870da86777bb3a77207c231
NakagamiYuta <guguru0014@gmail.com>
2018-07-18 15:23:47 +0900

    [change]DatabaseTableからDatabaseへパッケージ名を変更

---
+++ app/src/main/java/work/nityc_nyuta/mockline/Database/Tables.kt
@@ -0,0 +1,18 @@
+package work.nityc_nyuta.mockline.Database
+
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class TalkroomTable(
+        @PrimaryKey open var talkroomId: String = "",
+        open var talkroomName: String = "",
+        open var talkroomUserList: RealmList<String> = RealmList(),
+        open var talkHistory: RealmList<TalkHistoryTable> = RealmList()
+): RealmObject()
+
+open class TalkHistoryTable(
+        open var senderId: String = "",
+        open var message: String = "",
+        open var timestamp: Long = 0
+): RealmObject()
