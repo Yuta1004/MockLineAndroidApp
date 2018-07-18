3dc5f59522704a7e11838988c81ead2cbe7aa1aa
NakagamiYuta <guguru0014@gmail.com>
2018-07-19 02:00:56 +0900

    [add]新規トークを受信した時に通知を発行するように

--- app/src/main/java/work/nityc_nyuta/mockline/Firebase/MockLineFirebaseMessagingService.kt
+++ app/src/main/java/work/nityc_nyuta/mockline/Firebase/MockLineFirebaseMessagingService.kt
@@ -1,16 +1,23 @@
 package work.nityc_nyuta.mockline.Firebase
 
 import android.app.Activity
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.renderscript.Script
+import android.support.v4.app.NotificationCompat
 import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import work.nityc_nyuta.mockline.Activities.MyActivityLifeCycleCallbacks
+import work.nityc_nyuta.mockline.Activities.TalkActivity
 import work.nityc_nyuta.mockline.Database.TalkroomDatabaseHelper
+import work.nityc_nyuta.mockline.R
 import work.nityc_nyuta.mockline.ServerConncection.ServerConnectUserData
 import kotlin.concurrent.thread
 
@@ -24,12 +31,14 @@ class MockLineFirebaseMessagingService: FirebaseMessagingService(){
     }
 
     override fun onMessageReceived(remote_message: RemoteMessage?) {
+        // 送信者確認 & 送信データ確認
         if(remote_message!!.from != "1048318911529" || remote_message.data == null){
             return
         }
 
         val receiveData = remote_message.data!!
         val talkroomId = receiveData["talkroom_id"]!!
+        val talkroomName = receiveData["talkroom_name"]!!
         val senderId = receiveData["sender_id"]!!
         val message = receiveData["message"]!!
         val timestamp = receiveData["timestamp"]!!.toLong()
@@ -38,9 +47,14 @@ class MockLineFirebaseMessagingService: FirebaseMessagingService(){
         notifyReceiveNewMessage(talkroomId, senderId, message, timestamp)
 
         // DB保存
-        val databaseHelper = TalkroomDatabaseHelper()
+        val databaseHelper = TalkroomDatabaseHelper(applicationContext)
         databaseHelper.addTalkHistory(talkroomId, senderId, message, timestamp)
         databaseHelper.close()
+
+        // 現在表示中の画面がトークルームで，かつIDがtalkroomIDと同じ出ないなら通知を発行
+        if(talkroomId != MyActivityLifeCycleCallbacks.nowDisplayTalkroomID){
+            makeNotification(talkroomId, talkroomName, message)
+        }
     }
 
     override fun onNewToken(token: String?) {
@@ -64,5 +78,27 @@ class MockLineFirebaseMessagingService: FirebaseMessagingService(){
         sendIntent.action = "UpdateRecyclerView"
 
         notifyBroadCaster!!.sendBroadcast(sendIntent)
+    }
+
+    private fun makeNotification(talkroomId: String, talkroomName: String, message: String){
+        // 通知のビルダーと各種設定
+        val builder = NotificationCompat.Builder(applicationContext, talkroomId)
+        builder.setSmallIcon(R.drawable.ic_person_black_24dp)
+        builder.setContentTitle(talkroomName)
+        builder.setContentText(message)
+
+        // 通知が押された時のインテント
+        val talkIntent = Intent(applicationContext, TalkActivity::class.java)
+        talkIntent.putExtra("id", talkroomId)
+        talkIntent.putExtra("name", talkroomName)
+        val talkPendingIntent =
+                PendingIntent.getActivity(applicationContext, 0, talkIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+        builder.setContentIntent(talkPendingIntent)
+        builder.setAutoCancel(true)
+
+        // 通知発行
+        val notify = builder.build()
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(talkroomId, 0, notify)
     }
 }
